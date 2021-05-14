f21ae6bfb38093df65bd0e8bd1db55c029dff63c
Manny Morales <mannymorales@ip-10-57-8-59.ec2.internal>
2021-05-14 13:26:36 -0400

    Fixed errors

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ autoremove='autoremove';
 if [[ "$OSTYPE" == "linux-gnu"* ]];
 then
 	pkg='sudo apt';
-	pkgui='sudo apt';
+	pkgui='sudo apt install';
 	update='update';
 	upgrade='upgrade';
 	remove='remove';
@@ -19,14 +19,14 @@ then
 		/bin/bash -c "$( curl -fsSL $url )";
 	fi
 	pkg='brew';
-	pkgui='brew cask';
+	pkgui='brew install --cask';
 	update='update';
 	upgrade='upgrade';
 	remove='remove';
 elif [[ "$OSTYPE" == "freebsd"* ]];
 then
 	pkg='pkg';
-	pkgui='pkg';
+	pkgui='pkg install';
 	update='update';
 	upgrade='upgrade';
 	remove='delete';
@@ -35,35 +35,25 @@ fi;
 $pkg $update;
 $pkg $upgrade;
 
-# security
-$pkg $install nmap;
-$pkg $install wireshark;
-$pkg $install hexedit;
-
 # basics
-$pkg $install tmux;
-$pkg $install tree;
+pkgs=(tree tmux);
 
 # web
-$pkgui $install firefox;
-$pkg $install links;
+pkgsui=(firefox);
+pkgs=(${pkgs[@]} links);
 
 # development
-$pkg $install nasm;
-$pkg $install yasm;
-$pkg $install python3;
-$pkg $install node;
-$pkg $install html-xml-utils;
-$pkg $install jq;
+pkgs=(${pkgs[@]} nasm yasm python3 node html-xml-utils jq);
 
 # multimedia
-$pkg $install imagemagick;
-$pkg $install ffmpeg;
-$pkg $install youtube-dlc;
-$pkg $install mpv;
-$pkg $install aria2c;
-$pkg $install catt;
-$pkg $install exiftool;
+pkgs=(${pkgs[@]} imagemagick ffmpeg youtube-dlc mpv aria2 exiftool);
+
+# security -- careful with this on a company computer
+# ${pkgs[@]}=($pkgs nmap wireshark hexedit);
+
+# execute installs
+yes | $pkg install ${pkgs[@]};
+yes | $pkgui ${pkgsui[@]};
 
 ####### CONFIGS ########
 cp -nv vimrc ~/.vimrc;
